39327b097620198475ea97045c0081b63f9b3408
StasX <s.mestechkin@gmail.com>
2026-06-16 03:39:00 +0300

    Refactor EKS creation

---
+++ eks.tf
@@ -0,0 +1,50 @@
+# EKS Cluster
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = var.kubernetes_version
+
+  vpc_config {
+    subnet_ids              = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+  ]
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_node_group.arn
+  subnet_ids      = aws_subnet.private[*].id
+  instance_types  = var.instance_types
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_capacity
+    min_size     = var.min_capacity
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_policy,
+  ]
+
+  tags = {
+    Name = var.node_group_name
+  }
+}
--- vars.tf
+++ vars.tf
@@ -12,7 +12,50 @@ variable "AWS_REGION" {
   type      = string
 }
 
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
 variable "cluster_name" {
-  type    = string
-  default = "aws-monitor"
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+  default     = "my-eks-nodes"
+}
+
+variable "instance_types" {
+  description = "Instance types for the node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.33"  # Updated from "1.28" to "1.29"
+}
+
+variable "desired_capacity" {
+  description = "Desired number of nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_capacity" {
+  description = "Maximum number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "min_capacity" {
+  description = "Minimum number of nodes"
+  type        = number
+  default     = 1
 }
